b815b238d1859be5dfe6ed451d61621654f2351b
Brandon Trump <brandontrump101@gmail.com>
2023-07-16 01:24:58 +1200

    feat: completed clerk inital setup

---
+++ app/(auth)/(routes)/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,10 @@
+import { SignUp } from "@clerk/nextjs";
+import React from "react";
+
+export default function Page() {
+  return (
+    <div className="flex justify-center items-center h-full">
+      <SignUp />
+    </div>
+  );
+}
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,9 @@
+import { UserButton } from "@clerk/nextjs";
+
 export default function Home() {
-  return <main>Dashboard</main>;
+  return (
+    <main>
+      <UserButton afterSignOutUrl="/" />
+    </main>
+  );
 }
