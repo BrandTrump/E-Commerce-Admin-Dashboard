7bb3a6ff56847e945eb5551c36d0f6275650c956
Brandon Trump <brandontrump101@gmail.com>
2023-07-17 01:14:30 +1200

    feat: created navbar that displays the different stores, navigation links & user button

---
+++ components/Navbar.tsx
@@ -0,0 +1,32 @@
+import { UserButton, auth } from "@clerk/nextjs";
+import MainNav from "@/components/MainNav";
+import StoreSwitcher from "@/components/StoreSwitcher";
+import { redirect } from "next/navigation";
+import prismadb from "@/lib/prismadb";
+
+async function Navbar() {
+  const { userId } = auth();
+
+  if (!userId) {
+    redirect("/sign-in");
+  }
+
+  const stores = await prismadb.store.findMany({
+    where: {
+      userId,
+    },
+  });
+  return (
+    <div className="border-b">
+      <div className="flex h-16 items-center px-4">
+        <StoreSwitcher items={stores} />
+        <MainNav className="mx-6" />
+        <div className="ml-auto flex items-center space-x-4">
+          <UserButton afterSignOutUrl="/" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
