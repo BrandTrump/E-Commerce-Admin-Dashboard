bfe4f6b9d08bf0f7e8dc82c8f98c4d539e91313e
Brandon Trump <brandontrump101@gmail.com>
2023-07-16 19:33:25 +1200

    refactor: moved page into a routes folder

---
+++ app/(root)/(routes)/page.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { useStoreModal } from "@/hooks/use-store-modal";
+import { useEffect } from "react";
+
+export default function SetupPage() {
+  const [onOpen, isOpen] = useStoreModal((state) => [
+    state.onOpen,
+    state.isOpen,
+  ]);
+
+  useEffect(() => {
+    if (!isOpen) {
+      onOpen();
+    }
+  }, [isOpen, onOpen]);
+
+  return null;
+}
