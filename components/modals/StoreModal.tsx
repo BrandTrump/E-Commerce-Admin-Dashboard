ec3bcfa6f1f225656edec3547c6ad8287f1dc799
Brandon Trump <brandontrump101@gmail.com>
2023-07-16 14:46:47 +1200

    feat: created modal component that uses the state from the modal store

---
+++ components/modals/StoreModal.tsx
@@ -0,0 +1,21 @@
+import { useStoreModal } from "@/hooks/use-store-modal";
+import { Modal } from "@/components/ui/modal";
+
+function StoreModal() {
+  const [isOpen, onClose] = useStoreModal((state) => [
+    state.isOpen,
+    state.onClose,
+  ]);
+  return (
+    <Modal
+      title="Create Store"
+      description="Add a new store"
+      isOpen={isOpen}
+      onClose={onClose}
+    >
+      Future create store form
+    </Modal>
+  );
+}
+
+export default StoreModal;
