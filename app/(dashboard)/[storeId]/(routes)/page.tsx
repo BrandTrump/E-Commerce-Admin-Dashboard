b718659ac396ab5dbd86039f4cfd543a5b184fce
Brandon Trump <brandontrump101@gmail.com>
2023-07-16 20:52:40 +1200

    feat: created dashboard layout & dashboard page

---
+++ app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -0,0 +1,9 @@
+interface DashboardPageProps {
+  params: { storeId: string };
+}
+
+function DashboardPage() {
+  return <div>Dashboard</div>;
+}
+
+export default DashboardPage;
---
+++ app/(dashboard)/[storeId]/layout.tsx
@@ -0,0 +1,37 @@
+import prismadb from "@/lib/prismadb";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+async function layout({
+  children,
+  params,
+}: {
+  children: React.ReactNode;
+  params: { storeId: string };
+}) {
+  const { userId } = auth();
+
+  if (!userId) {
+    redirect("/sign-in");
+  }
+
+  const store = await prismadb.store.findFirst({
+    where: {
+      id: params.storeId,
+      userId,
+    },
+  });
+
+  if (!store) {
+    redirect("/");
+  }
+
+  return (
+    <>
+      <div>Navbar</div>
+      {children}
+    </>
+  );
+}
+
+export default layout;
