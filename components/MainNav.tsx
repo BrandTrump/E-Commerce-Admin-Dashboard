8eb39d9e79b449fc296c63114012c9b87ce24f3b
Brandon Trump <brandontrump101@gmail.com>
2023-07-17 01:15:26 +1200

    feat: maps through & displays the different routes

---
+++ components/MainNav.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+import { useParams, usePathname } from "next/navigation";
+
+function MainNav({ className, ...props }: React.HTMLAttributes<HTMLElement>) {
+  const pathName = usePathname();
+  const params = useParams();
+  const routes = [
+    {
+      href: `/${params.storeId}/settings`,
+      label: "Settings",
+      active: pathName === `/${params.storeId}/settings`,
+    },
+  ];
+  return (
+    <nav className={cn("flex items-center space-x-4 lg:space-x-6", className)}>
+      {routes.map((route) => (
+        <Link
+          key={route.href}
+          href={route.href}
+          className={cn(
+            "text-sm font-medium transition-colors hover:text-primary",
+            route.active
+              ? "text-black dark:text-white"
+              : "text-muted-foreground",
+            className
+          )}
+        >
+          {route.label}
+        </Link>
+      ))}
+    </nav>
+  );
+}
+
+export default MainNav;
