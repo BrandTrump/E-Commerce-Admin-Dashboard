04922b0c8aad98d3822adb9b4f8075290a6b01e1
Brandon Trump <brandontrump101@gmail.com>
2023-07-17 23:47:52 +1200

    feat: overview of billboards

---
+++ components/BillboardClient.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { Plus } from "lucide-react";
+import Heading from "./Heading";
+import { Button } from "./ui/button";
+import { Separator } from "./ui/separator";
+import { useParams, useRouter } from "next/navigation";
+
+function BillboardClient() {
+  const router = useRouter();
+  const params = useParams();
+  return (
+    <>
+      <div className="flex items-center justify-between">
+        <Heading
+          title="Billboards (0)"
+          description="Manage billboards for your store"
+        />
+        <Button
+          onClick={() => router.push(`/${params.storeId}/billboards/new`)}
+        >
+          <Plus className="mr-2 h-4 w-4" />
+          Add New
+        </Button>
+      </div>
+      <Separator />
+    </>
+  );
+}
+
+export default BillboardClient;
