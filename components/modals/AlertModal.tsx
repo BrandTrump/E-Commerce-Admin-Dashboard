1cd7b8540ea27c64093ba2eb86e10ac63111c5ee
Brandon Trump <brandontrump101@gmail.com>
2023-07-17 15:37:14 +1200

    feat: created an alert modal to prevent accidental deletes

---
+++ components/modals/AlertModal.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { Modal } from "../ui/modal";
+import { Button } from "../ui/button";
+
+interface AlertModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  onConfirm: () => void;
+  loading: boolean;
+}
+
+function AlertModal({ isOpen, onClose, onConfirm, loading }: AlertModalProps) {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) return;
+
+  return (
+    <Modal
+      title="Are you sure?"
+      description="This action cannot be undone."
+      isOpen={isOpen}
+      onClose={onClose}
+    >
+      <div className="pt-6 space-x-6 flex items-center justify-end w-full">
+        <Button disabled={loading} variant={"outline"} onClick={onClose}>
+          Cancel
+        </Button>
+        <Button disabled={loading} variant={"destructive"} onClick={onConfirm}>
+          Continue
+        </Button>
+      </div>
+    </Modal>
+  );
+}
+
+export default AlertModal;
