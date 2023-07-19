ee260e90df036b8f4bea0e3530170347d7dc07ca
Brandon Trump <brandontrump101@gmail.com>
2023-07-19 12:39:44 +1200

    feat: pass the formatted data as a prop to the billboard client component

--- app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
@@ -1,10 +1,28 @@
 import BillboardClient from "@/components/BillboardClient";
+import { BillboardColumn } from "@/components/Columns";
+import prismadb from "@/lib/prismadb";
+import { format } from "date-fns";
 
-function BillboardPage() {
+async function BillboardPage({ params }: { params: { storeId: string } }) {
+  const billboards = await prismadb.billboard.findMany({
+    where: {
+      storeId: params.storeId,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
+  const formatedBillboards: BillboardColumn[] = billboards.map((item) => ({
+    id: item.id,
+    label: item.label,
+    createdAt: format(item.createdAt, "MMMM do, yyyy"),
+  }));
+
   return (
     <div className="flex-col">
       <div className="flex-1 space-y-4 p-8 pt-6">
-        <BillboardClient />
+        <BillboardClient data={formatedBillboards} />
       </div>
     </div>
   );
