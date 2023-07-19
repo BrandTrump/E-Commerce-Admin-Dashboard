240ca9d6b517b7db1bb68bda6ff9860bfab686eb
Brandon Trump <brandontrump101@gmail.com>
2023-07-19 16:02:19 +1200

    feat: created new route for categories

---
+++ app/(dashboard)/[storeId]/(routes)/categories/page.tsx
@@ -0,0 +1,35 @@
+import { format } from "date-fns";
+import { CategoryColumn } from "./components/Columns";
+import prismadb from "@/lib/prismadb";
+import CategoryClient from "@/components/CategoryClient";
+
+async function CategoriesPage({ params }: { params: { storeId: string } }) {
+  const categories = await prismadb.category.findMany({
+    where: {
+      storeId: params.storeId,
+    },
+    include: {
+      billboard: true,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
+  const formatedCategories: CategoryColumn[] = categories.map((item) => ({
+    id: item.id,
+    name: item.name,
+    billboardLabel: item.billboard.label,
+    createdAt: format(item.createdAt, "MMMM do, yyyy"),
+  }));
+
+  return (
+    <div className="flex-col">
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        <CategoryClient data={formatedCategories} />
+      </div>
+    </div>
+  );
+}
+
+export default CategoriesPage;
