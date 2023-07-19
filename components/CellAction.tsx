628897ed1340b101bb96b3cd536ffd21d008fa05
Brandon Trump <brandontrump101@gmail.com>
2023-07-19 14:16:49 +1200

    feat: created cell action component to handle copying a billboard ID, updating a billboard & deleting a billboard

---
+++ components/CellAction.tsx
@@ -0,0 +1,87 @@
+"use client";
+
+import { Copy, Edit, MoreHorizontal, Trash } from "lucide-react";
+import { BillboardColumn } from "./Columns";
+import { Button } from "./ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
+import { toast } from "react-hot-toast";
+import { useParams, useRouter } from "next/navigation";
+import axios from "axios";
+import { useState } from "react";
+import AlertModal from "./modals/AlertModal";
+
+interface CellActionProps {
+  data: BillboardColumn;
+}
+
+function CellAction({ data }: CellActionProps) {
+  const router = useRouter();
+  const params = useParams();
+
+  const [loading, setLoading] = useState(false);
+  const [open, setOpen] = useState(false);
+
+  const onCopy = () => {
+    navigator.clipboard.writeText(data.id);
+    toast.success("Billboard id copied to clipboard.");
+  };
+
+  const onDelete = async () => {
+    try {
+      setLoading(true);
+      await axios.delete(`/api/${params.storeId}/billboards/${data.id}`);
+      router.refresh();
+      toast.success("Billboard deleted.");
+    } catch (error) {
+      toast.error("Make sure you removed all categories using this billboard.");
+    } finally {
+      setLoading(false);
+      setOpen(false);
+    }
+  };
+  return (
+    <>
+      <AlertModal
+        isOpen={open}
+        onClose={() => setOpen(false)}
+        onConfirm={onDelete}
+        loading={loading}
+      />
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant={"ghost"} className="h-8 w-8 p-0">
+            <span className="sr-only">Open menu</span>
+            <MoreHorizontal className="h-4 w-4" />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end">
+          <DropdownMenuLabel>Actions</DropdownMenuLabel>
+          <DropdownMenuItem onClick={onCopy}>
+            <Copy className="mr-2 h-4 w-4" />
+            Copy ID
+          </DropdownMenuItem>
+          <DropdownMenuItem
+            onClick={() =>
+              router.push(`/${params.storeId}/billboards/${data.id}`)
+            }
+          >
+            <Edit className="mr-2 h-4 w-4" />
+            Update
+          </DropdownMenuItem>
+          <DropdownMenuItem onClick={() => setOpen(true)}>
+            <Trash className="mr-2 h-4 w-4" />
+            Delete
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </>
+  );
+}
+
+export default CellAction;
