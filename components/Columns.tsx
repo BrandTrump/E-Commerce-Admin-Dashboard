f44253d75a76df11d05e8171d84816e745d579fe
Brandon Trump <brandontrump101@gmail.com>
2023-07-19 14:17:46 +1200

    feat: imported the cell action component

--- components/Columns.tsx
+++ components/Columns.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { ColumnDef } from "@tanstack/react-table";
+import CellAction from "./CellAction";
 
 export type BillboardColumn = {
   id: string;
@@ -16,5 +17,9 @@ export const columns: ColumnDef<BillboardColumn>[] = [
   {
     accessorKey: "createdAt",
     header: "Date",
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => <CellAction data={row.original} />,
   },
 ];
