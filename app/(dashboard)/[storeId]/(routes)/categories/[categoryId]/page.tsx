56fdc7968ec5c6aedde87105097d1bcf7ec3445c
Brandon Trump <brandontrump101@gmail.com>
2023-07-19 16:19:43 +1200

    feat: created category page that displays the category form

---
+++ app/(dashboard)/[storeId]/(routes)/categories/[categoryId]/page.tsx
@@ -0,0 +1,19 @@
+import CategoryForm from "@/components/CategoryForm";
+import prismadb from "@/lib/prismadb";
+
+async function CategoryPage({ params }: { params: { categoryId: string } }) {
+  const category = await prismadb.category.findUnique({
+    where: {
+      id: params.categoryId,
+    },
+  });
+  return (
+    <div className="flex-col">
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        <CategoryForm initialData={category} />
+      </div>
+    </div>
+  );
+}
+
+export default CategoryPage;
