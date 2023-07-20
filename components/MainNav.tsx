c71e3b963142e37efa3524ead7f334cc40a6876b
Brandon Trump <brandontrump101@gmail.com>
2023-07-20 16:42:22 +1200

    feat: added products route to the navigation bar

--- components/MainNav.tsx
+++ components/MainNav.tsx
@@ -34,6 +34,11 @@ function MainNav({ className, ...props }: React.HTMLAttributes<HTMLElement>) {
       active: pathName === `/${params.storeId}/colors`,
     },
     {
+      href: `/${params.storeId}/products`,
+      label: "Products",
+      active: pathName === `/${params.storeId}/products`,
+    },
+    {
       href: `/${params.storeId}/settings`,
       label: "Settings",
       active: pathName === `/${params.storeId}/settings`,
