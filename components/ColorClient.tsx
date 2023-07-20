47932a4eb6e0aae818bd02f983e0cb05e04bc1fe
Brandon Trump <brandontrump101@gmail.com>
2023-07-20 16:15:55 +1200

    feat: copied the size client/form & renamed the size values to color

---
+++ components/ColorClient.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import { Plus } from "lucide-react";
+import Heading from "@/components/Heading";
+import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
+import { useParams, useRouter } from "next/navigation";
+import {
+  ColorColumn,
+  columns,
+} from "@/app/(dashboard)/[storeId]/(routes)/colors/components/Columns";
+import { DataTable } from "@/components/ui/data-table";
+import ApiList from "@/components/ui/api-list";
+
+interface ColorClientProps {
+  data: ColorColumn[];
+}
+
+function ColorClient({ data }: ColorClientProps) {
+  const router = useRouter();
+  const params = useParams();
+  return (
+    <>
+      <div className="flex items-center justify-between">
+        <Heading
+          title={`Colors (${data.length})`}
+          description="Manage colors for your store"
+        />
+        <Button onClick={() => router.push(`/${params.storeId}/colors/new`)}>
+          <Plus className="mr-2 h-4 w-4" />
+          Add New
+        </Button>
+      </div>
+      <Separator />
+      <DataTable columns={columns} data={data} searchKey="name" />
+      <Heading title="API" description="API calls for colors" />
+      <Separator />
+      <ApiList entityName="colors" entityIdName="colorId" />
+    </>
+  );
+}
+
+export default ColorClient;
