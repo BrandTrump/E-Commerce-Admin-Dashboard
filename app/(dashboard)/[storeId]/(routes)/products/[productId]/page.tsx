4c796b19b104c54dfbaea92ebc7f986002bdf9ff
Brandon Trump <brandontrump101@gmail.com>
2023-07-21 15:50:02 +1200

    feat: created products route copied from the billboard route

---
+++ app/(dashboard)/[storeId]/(routes)/products/[productId]/page.tsx
@@ -0,0 +1,46 @@
+import ProductForm from "@/components/ProductForm";
+import prismadb from "@/lib/prismadb";
+
+async function ProductPage({
+  params,
+}: {
+  params: { productId: string; storeId: string };
+}) {
+  const product = await prismadb.product.findUnique({
+    where: {
+      id: params.productId,
+    },
+    include: {
+      images: true,
+    },
+  });
+  const categories = await prismadb.category.findMany({
+    where: {
+      storeId: params.storeId,
+    },
+  });
+  const sizes = await prismadb.size.findMany({
+    where: {
+      storeId: params.storeId,
+    },
+  });
+  const colors = await prismadb.color.findMany({
+    where: {
+      storeId: params.storeId,
+    },
+  });
+  return (
+    <div className="flex-col">
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        <ProductForm
+          initialData={product}
+          categories={categories}
+          sizes={sizes}
+          colors={colors}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default ProductPage;
---
+++ app/(dashboard)/[storeId]/(routes)/products/page.tsx
@@ -0,0 +1,43 @@
+import { ProductColumn } from "@/app/(dashboard)/[storeId]/(routes)/products/components/Columns";
+import prismadb from "@/lib/prismadb";
+import { format } from "date-fns";
+import { formatter } from "@/lib/utils";
+import ProductClient from "@/components/ProductClient";
+
+async function ProductsPage({ params }: { params: { storeId: string } }) {
+  const products = await prismadb.product.findMany({
+    where: {
+      storeId: params.storeId,
+    },
+    include: {
+      category: true,
+      size: true,
+      color: true,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
+  const formatedProducts: ProductColumn[] = products.map((item) => ({
+    id: item.id,
+    name: item.name,
+    isFeatured: item.isFeatured,
+    isArchived: item.isArchived,
+    price: formatter.format(item.price.toNumber()),
+    category: item.category.name,
+    size: item.category.name,
+    color: item.color.value,
+    createdAt: format(item.createdAt, "MMMM do, yyyy"),
+  }));
+
+  return (
+    <div className="flex-col">
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        <ProductClient data={formatedProducts} />
+      </div>
+    </div>
+  );
+}
+
+export default ProductsPage;
