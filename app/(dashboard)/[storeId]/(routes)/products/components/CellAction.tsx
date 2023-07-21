cd06d1c3f14ce97e451c7785d0e3064242065d52
Brandon Trump <brandontrump101@gmail.com>
2023-07-21 22:34:28 +1200

    feat: changed to products api routes

--- app/(dashboard)/[storeId]/(routes)/products/components/CellAction.tsx
+++ app/(dashboard)/[storeId]/(routes)/products/components/CellAction.tsx
@@ -29,17 +29,17 @@ function CellAction({ data }: CellActionProps) {
 
   const onCopy = () => {
     navigator.clipboard.writeText(data.id);
-    toast.success("Billboard id copied to clipboard.");
+    toast.success("Product id copied to clipboard.");
   };
 
   const onDelete = async () => {
     try {
       setLoading(true);
-      await axios.delete(`/api/${params.storeId}/billboards/${data.id}`);
+      await axios.delete(`/api/${params.storeId}/products/${data.id}`);
       router.refresh();
-      toast.success("Billboard deleted.");
+      toast.success("Product deleted.");
     } catch (error) {
-      toast.error("Make sure you removed all categories using this billboard.");
+      toast.error("Something went wrong.");
     } finally {
       setLoading(false);
       setOpen(false);
@@ -68,7 +68,7 @@ function CellAction({ data }: CellActionProps) {
           </DropdownMenuItem>
           <DropdownMenuItem
             onClick={() =>
-              router.push(`/${params.storeId}/billboards/${data.id}`)
+              router.push(`/${params.storeId}/products/${data.id}`)
             }
           >
             <Edit className="mr-2 h-4 w-4" />
