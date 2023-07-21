66ad07298c253960f056d0d7057c952d95153c8a
Brandon Trump <brandontrump101@gmail.com>
2023-07-21 23:12:49 +1200

    feat: added orders route to naviagation

--- components/MainNav.tsx
+++ components/MainNav.tsx
@@ -39,6 +39,11 @@ function MainNav({ className, ...props }: React.HTMLAttributes<HTMLElement>) {
       active: pathName === `/${params.storeId}/products`,
     },
     {
+      href: `/${params.storeId}/orders`,
+      label: "Orders",
+      active: pathName === `/${params.storeId}/products`,
+    },
+    {
       href: `/${params.storeId}/settings`,
       label: "Settings",
       active: pathName === `/${params.storeId}/settings`,
