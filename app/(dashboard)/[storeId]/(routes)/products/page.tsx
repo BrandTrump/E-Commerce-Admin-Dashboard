70cc7099ddded83748ee5fdb4ab4203e95a11a1c
Brandon Trump <brandontrump101@gmail.com>
2023-07-21 22:33:52 +1200

    fix: changed format to display correct size name

--- app/(dashboard)/[storeId]/(routes)/products/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/products/page.tsx
@@ -26,7 +26,7 @@ async function ProductsPage({ params }: { params: { storeId: string } }) {
     isArchived: item.isArchived,
     price: formatter.format(item.price.toNumber()),
     category: item.category.name,
-    size: item.category.name,
+    size: item.size.name,
     color: item.color.value,
     createdAt: format(item.createdAt, "MMMM do, yyyy"),
   }));
