00ad8768a2d5881a8293e52db1643c4735bbd95e
Brandon Trump <brandontrump101@gmail.com>
2023-07-24 23:33:48 +1200

    fix: included the billboard inside of the get function

--- app/api/[storeId]/categories/[categoryId]/route.ts
+++ app/api/[storeId]/categories/[categoryId]/route.ts
@@ -108,6 +108,9 @@ export async function GET(
       where: {
         id: params.categoryId,
       },
+      include: {
+        billboard: true,
+      },
     });
 
     return NextResponse.json(category);
