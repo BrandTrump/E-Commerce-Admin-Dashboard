0735370b565a08748584b49db227c284b94f5636
Brandon Trump <brandontrump101@gmail.com>
2023-07-26 16:19:36 +1200

    feat: included the dark mode toggle inside of the navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,6 +3,7 @@ import MainNav from "@/components/MainNav";
 import StoreSwitcher from "@/components/StoreSwitcher";
 import { redirect } from "next/navigation";
 import prismadb from "@/lib/prismadb";
+import { DarkModeToggle } from "./DarkModeToggle";
 
 async function Navbar() {
   const { userId } = auth();
@@ -22,6 +23,7 @@ async function Navbar() {
         <StoreSwitcher items={stores} />
         <MainNav className="mx-6" />
         <div className="ml-auto flex items-center space-x-4">
+          <DarkModeToggle />
           <UserButton afterSignOutUrl="/" />
         </div>
       </div>
